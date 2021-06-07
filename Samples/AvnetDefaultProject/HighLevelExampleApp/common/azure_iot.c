005981b21d60cf3048dd2a07ed50e6e4dd976b3e
Brian Willess <brian.willess@avnet.com>
2021-06-07 08:34:05 -0400

    IoTConnect improvements

--- Samples/AvnetDefaultProject/HighLevelExampleApp/common/azure_iot.c
+++ Samples/AvnetDefaultProject/HighLevelExampleApp/common/azure_iot.c
@@ -179,11 +179,6 @@ static void AzureTimerEventHandler(EventLoopTimer *timer)
         if ((status & Networking_InterfaceConnectionStatus_ConnectedToInternet) &&
             (iotHubClientAuthenticationState == IoTHubClientAuthenticationState_NotAuthenticated)) {
             SetUpAzureIoTHubClient();
-
-#ifdef USE_IOT_CONNECT
-            // Kick off the IoTConnect specific logic since we're connected!
-            IoTConnectConnectedToIoTHub();
-#endif            
         }
     } else {
         if (errno != EAGAIN) {
